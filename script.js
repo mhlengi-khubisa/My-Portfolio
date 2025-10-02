6d3e4cc6796fd19402f7c0c30f2f16195b747f87
Mhlengi Khubisa <82275571+mhlengi-khubisa@users.noreply.github.com>
2025-10-02 17:17:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+firstText = "Software Developer.";
+secondText = "Web Developoer.";
+
+intervalTime = 600;
+window.load = displayText();
+function displayText() {
+    // display first text
+    document.querySelector('#dynamicContent').innerText = firstText;
+    // display second text
+    setTimeout(() => {
+        document.querySelector('#dynamicContent').innerText = secondText;
+    }, intervalTime * 3);
+    // display third text
+    setTimeout(() => {
+        document.querySelector('#dynamicContent').innerText = thirdText;
+    }, intervalTime * 5);
+}
+
+setInterval(() => {
+    displayText();
+}, intervalTime * 7);
+
